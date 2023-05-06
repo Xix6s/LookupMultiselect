bbea3aebf317994f3931462ab6bdeec663cd6ef7
raulrrdwdcefcerfvsasn <raul@xix6s.comfgvfevcf>
2023-05-05 23:51:37 -0400

    first commit

---
+++ LookupMultiselect/MultiselectRecords.types.tsx
@@ -0,0 +1,44 @@
+import { IGroup } from "@fluentui/react";
+import { IInputs } from "./generated/ManifestTypes";
+
+export interface IColumnObject {
+    key: string,
+    name: string,
+    fieldName: string,
+    minWidth: number,
+    maxWidth?: number,
+    isResizable: boolean
+}
+
+export interface IMultiselectProps {
+    openWindow: any;
+    engagementId: any;
+    width: string | number | undefined;
+    eventOnChangeValue(textFieldValue: string): void;
+    triggerFilter(searchValue: string) : void;
+    attributeid: any;
+    data: any;
+    requestUrl: string,
+    authorizationToken: string,
+    userInput?: string;
+    userInputChanged: (newValue: string) => void,
+    labid: string,
+    widthColumn: number,
+    columns: [IColumnObject],
+    groups: IGroup[];
+    inputValue?: string,
+    dataToBeSet: string,
+    headerVisible: boolean,
+    isControlVisible: boolean,
+    isControlDisabled: boolean,
+    populatedFieldVisible: boolean,
+    records: any,
+    logicalName: string,
+    widthProp: number,
+    heightProp: number,
+    filterTags: boolean,
+    numberIfRecordsToBeShown: number,
+    context: ComponentFramework.Context<IInputs>,
+    isMultiple: boolean,
+    groupBy: string
+}
